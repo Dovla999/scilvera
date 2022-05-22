81a027ce04540b46d58d72ee6534699bf5f7a830
Dovla999 <vladimirjovin8@gmail.com>
2022-05-22 10:22:37 +0200

    java compiled with auth

---
+++ scilvera_implementation/scipubsi/output/User/src/main/java/com/silvera/User/auth/TokenAuthenticationFilter.java
@@ -0,0 +1,74 @@
+package com.silvera.User.auth;
+
+
+import java.io.IOException;
+import java.util.Collections;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import com.silvera.User.utils.TokenUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.ExpiredJwtException;
+public class TokenAuthenticationFilter extends BasicAuthenticationFilter {
+
+
+    private UserDetailsService userDetailsService;
+
+    protected final Log LOGGER = LogFactory.getLog(getClass());
+    private final TokenUtils jwtTokenUtils;
+
+    public TokenAuthenticationFilter(AuthenticationManager authManager, TokenUtils tokenUtils) {
+        super(authManager);
+        this.jwtTokenUtils = tokenUtils;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        // retrieve request authorization header
+        final String authorizationHeader = req.getHeader("Authorization");
+
+        // authorization header must be set and start with Bearer
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+
+
+
+            final String username = jwtTokenUtils.getUsernameFromToken(authorizationHeader);
+
+            // if user e-mail has been retrieved correctly from the token and if user is not already authenticated
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+                // authenticate user
+                final UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null);
+
+                // set authentication in security context holder
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            }
+        }
+        // no token specified
+        else {
+            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+        // pass request down the chain, except for OPTIONS requests
+        if (!"OPTIONS".equalsIgnoreCase(req.getMethod())) {
+            chain.doFilter(req, res);
+        }
+
+    }
+
+}
---
+++ scilvera_implementation/scipubsi/output/User/src/main/java/com/silvera/User/config/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.silvera.User.config;
+
+import com.silvera.User.auth.CustomAuthenticationManager;
+import com.silvera.User.auth.TokenAuthenticationFilter;
+import com.silvera.User.utils.TokenUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+
+    @Autowired
+    private TokenUtils jwtTokenUtils;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // disable CSRF as we do not serve browser clients
+                .csrf().disable()
+                // match GraphQL endpoint
+                .antMatcher("/graphql")
+                // add JWT authorization filter
+                .addFilter(new TokenAuthenticationFilter(new CustomAuthenticationManager(), jwtTokenUtils))
+
+                // allow access restriction using request matcher
+                .authorizeRequests()
+                // authenticate requests to GraphQL endpoint
+                .antMatchers("/graphql").authenticated()
+                // allow all other requests
+                .anyRequest().permitAll().and()
+                // make sure we use stateless session, session will not be used to store user's state
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+}
